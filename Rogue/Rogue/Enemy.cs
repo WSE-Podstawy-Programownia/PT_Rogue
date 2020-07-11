4220a3975bae21c737f48342d62b00c2847f2c05
tangosh <tangosh42@gmail.com>
2020-07-11 14:43:43 +0200

    Random enemy movement

--- Rogue/Rogue/Enemy.cs
+++ Rogue/Rogue/Enemy.cs
@@ -19,6 +19,7 @@ namespace Rogue
 
 		public int HitPoints { get; set; } = 5;
 		public Tile StoodOnTile { get; set; }
+
 		Random random = new Random();
 		public override char Character { get; set; } = 'E';
 
@@ -29,10 +30,6 @@ namespace Rogue
 		public override int PositionX { get; set; }
 
 
-		public void Move(int moveToY, int moveToX)
-		{
-
-		}
 
 		public virtual void AIBehaviour()
 		{
@@ -45,11 +42,47 @@ namespace Rogue
 				{
 				Attack(Game.player);
 				} 
-				else
-				{ 
-				//implement movement towards player
+			else
+				{
+				int desiredY = PositionY;
+				int desiredX = PositionX;
+				//Determine direction
+				int roll = random.Next(0, 4);
+				switch (roll)
+				{
+					case 0:
+						desiredY++;
+						break;
+					case 1:
+						desiredY--;
+						break;
+					case 2:
+						desiredX++;
+						break;
+					case 3:
+						desiredX--;
+						break;
 				}
 
+				if (Game.map.Room[desiredY, desiredX].Walkable == true)
+				{
+					Move(desiredY, desiredX);
+				}
+			}
+
+		}
+
+		public void Move(int moveToY, int moveToX)
+		{
+			//replace pre-move position Tile on the map
+			Game.map.Room[PositionY, PositionX] = StoodOnTile;
+
+			//Change position to new values and change the Tile on the Map to player
+			PositionY = moveToY;
+			PositionX = moveToX;
+
+			StoodOnTile = Game.map.Room[PositionY, PositionX];
+			Game.map.Room[PositionY, PositionX] = this;
 		}
 
 		public virtual void Attack(IFightable target)
